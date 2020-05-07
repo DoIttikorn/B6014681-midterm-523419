39eaa281d494a12f23964b2b49e1be03d45c32b7
DoIttikorn <aseaways1234@gmail.com>
2020-05-07 09:49:50 +0700

    no 1

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -1,41 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-interface user_profile{
-  studentid: string,
-  name: string,
-  gender: string,
-  birthyear: number,
-  email: string,
-  phone: string,
-  img: string,
-  address: string,
-  comment: string
+interface user_profile {
+  studentid: string;
+  name: string;
+  gender: string;
+  birthyear: number;
+  email: string;
+  phone: string;
+  img: string;
+  address: string;
+  comment: string;
 }
 
 @Component({
-  selector: 'app-first',
-  templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  selector: "app-first",
+  templateUrl: "./first.component.html",
+  styleUrls: ["./first.component.css"],
 })
 export class FirstComponent implements OnInit {
+  profile: user_profile;
 
-  profile: user_profile
-
-  constructor() { 
+  constructor() {
     this.profile = {
-      studentid: '',
-      name: '',
-      gender: '',
-      birthyear: 0,
-      email: '',
-      phone: '',
-      img: 'https://api.adorable.io/avatars/285/abott@adorable.io.png',
-      address: 'SUT 111 University Avenue, Suranaree Sub-Distric, Muang Nakhon Ratchasima Distric, Nakhon Ratchasima 30000 Thailand',
-      comment: ''
-    }
+      studentid: "B6014681",
+      name: "Ittikorn Chawkamud",
+      gender: "Male",
+      birthyear: 1998,
+      email: "aseaways1234@gmail.com",
+      phone: "093-5281333",
+      img:
+        "https://avatars0.githubusercontent.com/u/57912233?s=460&u=7d1ec3616ef80f56d7406520b14db3d9a7e6aa8b&v=4",
+      address: "225 No.3,Tambon  banlaim , Amphur Muang,Udonthai 41000",
+      comment: "",
+    };
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  showAddress() {
+    alert(this.profile.address);
+  }
 }
