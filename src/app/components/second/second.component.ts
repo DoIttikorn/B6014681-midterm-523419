c5b5d6c08ce5405c287e87e8a3b4d5fdee513198
DoIttikorn <aseaways1234@gmail.com>
2020-05-07 10:19:19 +0700

    no 2

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, FormArray, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-second',
-  templateUrl: './second.component.html',
-  styleUrls: ['./second.component.css']
+  selector: "app-second",
+  templateUrl: "./second.component.html",
+  styleUrls: ["./second.component.css"],
 })
 export class SecondComponent implements OnInit {
-
   productForm = new FormGroup({
-    
+    type: new FormControl(""),
+    id: new FormControl(""),
+    name: new FormControl(""),
+    detail: new FormControl(""),
+    quantity: new FormControl("", [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(50),
+    ]),
+    price: new FormControl("", [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(100000),
+    ]),
   });
 
-  productType: string[] = ['CPU','RAM','HDD','Mainboard'];
+  productType: string[] = ["CPU", "RAM", "HDD", "Mainboard"];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get quantity() {
+    return this.productForm.get("quantity");
   }
-
+  get price() {
+    return this.productForm.get("price");
+  }
+  OK() {
+    return this.productForm.status;
+  }
+  addProduct() {
+    
+  }
 }
